0eff4de5bd9c596cfd90ebd834dd06fcbef5a64e
kawai011231 <yousuke011231@gmail.com>
2022-09-15 18:57:22 +0900

    ページ追加しました

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = '河合　耀介';
+export const siteTitle = 'my profile';
 
 export default function Layout({ children, home }) {
     return (
--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>河合耀介のプロフィールサイトです</p>
+        <p>質素なサイトですみません、、、このプロフィールサイトはNext.jsの公式チュートリアルに沿って作成し、少し改良を加えました。</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>my-site</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
@@ -42,6 +39,14 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
+        <div className={utilStyles.achievement}>
+          <p>実績</p>
+          <a href='https://fukui-hougen.net/'>福井の方言愛着ましましプロジェクト</a>
+          <a href='https://koge-school.com/'>KOGE塾様</a>
+          <a href='https://unsung-hair.com/'>美容室unsung様</a>
+          <a href='https://meme-sns.com/'>株式会社meme様</a>
+          <a href='https://my-todo-vert.vercel.app/?'>Reactでtodoアプリ作ってみました</a>
+        </div>
       </section>
     </Layout>
   );
